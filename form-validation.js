54de199356c821c5ba8ae2926df2483e0b4f1613
Michael Crean <mcrean1@mail.csuchico.edu>
2016-10-10 17:48:56 -0700

    added variables and made it so the two rules apply: user greater than six and password cannot equal password.

--- form-validation.js
+++ form-validation.js
@@ -2,18 +2,21 @@ function validate(){
   //Grab the user's input and store in variables
   var userEntered = document.getElementById("user").value;
   var passEntered = document.getElementById("pass").value;
+  var txt = userEntered.length;
+  if( txt < 6){
+document.getElementById("usernamerError").innerHTML="Bad username.";
+document.getElementById("usernameError").classList.remove("hidden-message");
+document.getElementById("usernameError").classList.add("shown-message");
+document.getElementById("usernameGroup").classList.add("has-error");
+}
 
-  //Show message that there is an error with the username...
-  document.getElementById("usernameError").innerHTML="Bad username.";
-  document.getElementById("usernameError").classList.remove("hidden-message");
-  document.getElementById("usernameError").classList.add("shown-message");
-  //Turn the username items red
-  document.getElementById("usernameGroup").classList.add("has-error");
 
-  //Show message that there is an error with the password...
+
+var password = "password";
+if(passEntered == password){
   document.getElementById("passwordError").innerHTML="Bad password.";
   document.getElementById("passwordError").classList.remove("hidden-message");
   document.getElementById("passwordError").classList.add("shown-message");
-  //Turn the password items red
   document.getElementById("passwordGroup").classList.add("has-error");
+}
 }
